14b70a1728c709149263655d7379da05d363933e
Larry Chiem <larrychiem@gmail.com>
2022-11-08 14:34:32 -0800

    Update ggwp.sh

--- ggwp.sh
+++ ggwp.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 mkdir /volumes/untitled/tmp
-mkdir /volumes/untitled/tmp/applications
 cd /volumes/untitled/tmp
 sleep 5
 curl -OL --progress-bar https://swcdn.apple.com/content/downloads/36/13/012-90254-A_BJQ1VMPD44/z225i7bzise31eo21e4kgfkafz7zq0q9tu/InstallAssistant.pkg
