f7feb6e9931b18ba83b75f232eabd2a700e04fd9
Larry Chiem <larrychiem@gmail.com>
2024-01-31 10:32:50 -0800

    Update ggwp.sh

--- ggwp.sh
+++ ggwp.sh
@@ -3,7 +3,7 @@ mkdir /volumes/untitled/tmp
 cd /volumes/untitled/tmp
 sleep 5
 ECHO "Downloading MacOS Monterey Install Assistant.pkg"
-curl -OL --progress-bar https://swcdn.apple.com/content/downloads/17/14/032-33828-A_KX25OQBX8S/lz9yip4rmenyju8nkv4wpaqtdmvzfogkcb/InstallAssistant.pkg
+curl -OL --progress-bar https://swcdn.apple.com/content/downloads/53/08/052-33037-A_AKHX79ZA4S/z7yb5wdcrk453a3hi7c3hc9n6zzju9di7f/InstallAssistant.pkg
 sleep 5
 ECHO "Installing Monterey Install Assistant @ /volumes/Untitled/Applications/"
 installer -verboseR -pkg /volumes/untitled/tmp/InstallAssistant.pkg -target "/volumes/untitled/"
